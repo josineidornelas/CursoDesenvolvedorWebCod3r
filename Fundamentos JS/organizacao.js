bc1800782ba279c16a7fb664240d1b2c5f1f973e
josineidornelas <josineidornelas@hotmail.com>
2020-12-25 03:13:30 -0300

    início

---
+++ Fundamentos JS/organizacao.js
@@ -0,0 +1,10 @@
+console.log("Sentença de Código") //pode terminar ou não com ";".
+
+{
+    {
+        console.log("Olá ")
+        console.log('Mundo!')
+    }
+}
+
+//O JavaScript é organizado em sentenças de códigos e blocos de código
---
+++ README.md
@@ -0,0 +1,7 @@
+# Aprendendo Desenvolvimento Web
+
+
+
+- HTML 5;
+- CSS 3;
+- JavaScript.
