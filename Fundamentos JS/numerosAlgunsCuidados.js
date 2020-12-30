d3ff793aa62a591d3dfb4d5478277fea037404ef
josineidornelas <josineidornelas@hotmail.com>
2020-12-30 01:19:16 -0300

    30/12/2020

---
+++ Fundamentos JS/numerosAlgunsCuidados.js
@@ -0,0 +1,7 @@
+console.log(7 / 0) //retorna infinito
+console.log("10" / 2)
+console.log("10,2" / 2)
+console.log("show" * 2)
+console.log(8.1 + 0,7)
+//console.log(10.toString()) não funciona
+console.log((10.465786).toFixed(2))
