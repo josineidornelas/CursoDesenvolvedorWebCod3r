30eda02e37cb5167918ec10da0f1090590d78ecf
josineidornelas <josineidornelas@hotmail.com>
2021-01-18 03:04:28 -0300

    atualizacao

---
+++ Fundamentos JS/parNomeValor.js
@@ -0,0 +1,22 @@
+//par nome/valor
+const saudacao = 'opa' //contexto lexico 1
+
+function exec() {
+    const saudacao = 'Falaaa' //contexto lexico 2
+    return saudacao
+}
+
+//Objetos são grupos aninhados de pares nome/valor
+const cliente = {
+    nome: 'Pedro',
+    idade: 32,
+    peso: 120,
+    endereco: {
+        logradouro: 'Rua muito legal',
+        numero: 123
+    }
+}
+
+console.log(saudacao)
+console.log(exec())
+console.log(cliente)
