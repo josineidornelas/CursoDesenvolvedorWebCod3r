2b43cb4717ae919dd54c5a07061395e5554d84fa
josineidornelas <josineidornelas@hotmail.com>
2021-02-23 08:33:59 -0300

    atualização

---
+++ Funcao/IIFE.js
@@ -0,0 +1,6 @@
+//Immediately Invoked Fonction Expression
+
+(function(){
+    console.log('Será executado na hora!')
+    console.log('Foge do escopo mais abrangente.')
+})()
---
+++ Funcao/factory2.js
@@ -0,0 +1,10 @@
+function criarProduto(nome, preco) {
+    return {
+        nome,
+        preco,
+        desconto: 0.1
+    }
+}
+
+console.log(criarProduto('notebook', 1199.98))
+console.log(criarProduto('iPad', 1199.37))
